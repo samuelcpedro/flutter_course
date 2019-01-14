af939dff622c2cd5a8cd07cda6e7ca0625dbb90d
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-14 21:24:37 -0100

    Splitting our Code Up Section 2, Lecture 30

--- lib/main.dart
+++ lib/main.dart
@@ -41,19 +41,7 @@ class _MyAppState extends State<MyApp> {
                     child: Text('Add Product'),
                   ),
                 ),
-                Column(
-                  children: _products
-                      .map((element) =>
-                      Card(
-                        child: Column(
-                          children: <Widget>[
-                            Image.asset('assets/food.jpg'),
-                            Text(element)
-                          ],
-                        ),
-                      ))
-                      .toList(),
-                )
+                ////sdgasdfasdfasdfasdfasdf
               ],
             )));
   }
