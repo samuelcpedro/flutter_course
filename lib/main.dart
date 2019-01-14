bd1b8856a0a33fb2068403f717fa8d5aa30a770a
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-14 20:41:50 -0100

    Creating a Stateful Widget Section 2, Lecture 27

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,15 @@ void main() {
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
