3a681e789cb456822fb7902240c4bdaa2f01bcd5
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-14 20:31:55 -0100

    Adding Cards & Images Section 2, Lecture 24 Adding a Button Section 2, Lecture 26

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/food.jpg'),
-          Text('Food Paradise')
-        ],),),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('EasyList'),
+            ),
+            body: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text('Add Product'),
+                  ),
+                ),
+                Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/food.jpg'),
+                      Text('Food Paradise')
+                    ],
+                  ),
+                )
+              ],
+            )));
   }
 }
