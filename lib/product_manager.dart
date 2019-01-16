d56883111fd375818c95ef097a2036f2419db48b
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-16 21:05:44 -0100

    initState() and super.initState() Section 2, Lecture 33
    In the last lecture, initState() was introduced. You learned that you have to call super.initState() in there and that you should put it at the end of your initState() implementation.
    
    It's now actually recommend to move at at the beginning, though it will not influence the way your code runs. So the code shown in this course will still work fine without any issues.
    
    But to follow the official recommendation and code style, you should use this code:
    
        @override
          void initState() {
            super.initState();
            _products.add(widget.startingProduct);
          }

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -19,19 +19,29 @@ class ProductManager extends StatefulWidget {
 
 //
 class _ProductManagerState extends State<ProductManager> {
-  // the _ character is to define something that will only be used inside of this class
+  // the _ character is to define something that will only
+  // be used inside of this class
   List<String> _products = [];
 
   //
   @override
   void initState() {
+    /**
+     * In the last lecture, initState() was introduced.
+     * You learned that you have to call super.initState() in there and that
+     * you should put it at the end of your initState() implementation.
+     *
+     * It's now actually recommend to move at at the beginning,
+     * though it will not influence the way your code runs.
+     * So the code shown in this course will still work fine without any issues.
+     */
+    super.initState();
     // widget is provided by the state object and gives us access to the
     // connected StatefulWidget and to its properties and therefore I can
     // safely access startingProduct, add it to my _products and
     // initState will execute when this state is created, so when build runs
     // for the first time, that code will already have been executed.
     _products.add(widget.startingProduct);
-    super.initState();
   }
   
   @override
